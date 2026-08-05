875b4ca38159e561002df0d53d9b37dac7889032
Khiem Vu <bell.vu2005@gmail.com>
2026-08-05 14:33:14 -0700

    Improved analysis notebooks, reorganize folders

--- plotter.py
+++ plotter.py
@@ -62,7 +62,7 @@ def get_radar(addr):
 
     return radars[addr]
 
-log_file = open("data_logs/mobile_field_dataset_calibrated/mobile_12_3.txt", "a", buffering=1)  # line-buffered
+log_file = open("data_logs/mobile_field_dataset_calibrated/bed1/mobile_20_3.txt", "a", buffering=1)  # line-buffered
 #8 speed
 
 while True:
